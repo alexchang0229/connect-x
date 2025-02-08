27d3e975d517e1c87a1d39addb6d3f07b94167e5
Samuel Beaubien <samuel.beaubien@gmail.com>
2025-02-08 09:56:29 -0500

    Clean up

--- run_matchup_week_1.py
+++ run_matchup_week_1.py
@@ -34,17 +34,17 @@ player_3 = random_agent_3
 # MATCHUP 1 and 2
 matchup_1_2: ConnectXMatchup = ConnectXMatchup(7, 6, 4, player_1_name, player_2_name, player_1, player_2, 2, 5, 100)
 matchup_1_2.play_matchup()
-matchup_1_2.generate_report(f"{player_1_name}_vs_{player_2_name}.txt")
+matchup_1_2.generate_report(f"out/{player_1_name}_vs_{player_2_name}.txt")
 
 # MATCHUP 1 and 3
 matchup_1_3: ConnectXMatchup = ConnectXMatchup(7, 6, 4, player_1_name, player_3_name, player_1, player_3, 2, 5, 100)
 matchup_1_3.play_matchup()
-matchup_1_3.generate_report(f"{player_1_name}_vs_{player_3_name}.txt")
+matchup_1_3.generate_report(f"out/{player_1_name}_vs_{player_3_name}.txt")
 
 # MATCHUP 2 and 3
 matchup_2_3: ConnectXMatchup = ConnectXMatchup(7, 6, 4, player_2_name, player_3_name, player_2, player_3, 2, 5, 100)
 matchup_2_3.play_matchup()
-matchup_2_3.generate_report(f"{player_2_name}_vs_{player_3_name}.txt")
+matchup_2_3.generate_report(f"out/{player_2_name}_vs_{player_3_name}.txt")
 
 
 
